2ee1bdddfaf8889521a6524fda076ffa2546708d
Matthew Gaudio <mattgaud2425@Gmail.com>
2019-07-24 17:29:53 -0400

    initial commit

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+gem 'clearance'
+gem 'administrate'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: "pages#main"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+   # Add dashboard for your models here
+   resources :users
+
+   root to: "users#index" # <--- Root route
+ end
 end
